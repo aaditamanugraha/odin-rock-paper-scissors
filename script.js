b97343a321bc1710f14a554fb9d1adb5a966fbe8
aaditamanugraha <adhityaaditaman@gmail.com>
2023-02-26 23:34:29 +0700

    Update playRound() and create play game function

--- script.js
+++ script.js
@@ -11,31 +11,34 @@ function playRound(player) {
   let computerSelection = getComputerChoice();
 
   if (playerSelection === "ROCK" && computerSelection === "SCISSORS") {
-    console.log("You win! " + playerSelection + " beats " + computerSelection);
+    alert("You win! " + playerSelection + " beats " + computerSelection);
   } else if (playerSelection === "ROCK" && computerSelection === "PAPER") {
-    console.log("You lose! " + computerSelection + " beats " + playerSelection);
+    alert("You lose! " + computerSelection + " beats " + playerSelection);
   } else if (playerSelection === "ROCK" && computerSelection === "ROCK") {
-    console.log(
-      "Draw! " + computerSelection + " can't beats " + playerSelection
-    );
+    alert("Draw! " + computerSelection + " can't beats " + playerSelection);
   } else if (playerSelection === "PAPER" && computerSelection === "ROCK") {
-    console.log("You win! " + playerSelection + " beats " + computerSelection);
+    alert("You win! " + playerSelection + " beats " + computerSelection);
   } else if (playerSelection === "PAPER" && computerSelection === "SCISSORS") {
-    console.log("You lose! " + computerSelection + " beats " + playerSelection);
+    alert("You lose! " + computerSelection + " beats " + playerSelection);
   } else if (playerSelection === "PAPER" && computerSelection === "PAPER") {
-    console.log(
-      "Draw! " + computerSelection + " can't beats " + playerSelection
-    );
+    alert("Draw! " + computerSelection + " can't beats " + playerSelection);
   } else if (playerSelection === "SCISSORS" && computerSelection === "PAPER") {
-    console.log("You win! " + playerSelection + " beats " + computerSelection);
+    alert("You win! " + playerSelection + " beats " + computerSelection);
   } else if (playerSelection === "SCISSORS" && computerSelection === "ROCK") {
-    console.log("You lose! " + computerSelection + " beats " + playerSelection);
+    alert("You lose! " + computerSelection + " beats " + playerSelection);
   } else if (
     playerSelection === "SCISSORS" &&
     computerSelection === "SCISSORS"
   ) {
-    console.log(
-      "Draw! " + computerSelection + " can't beats " + playerSelection
-    );
+    alert("Draw! " + computerSelection + " can't beats " + playerSelection);
   }
+
+  return;
+}
+
+function game() {
+  let userInput = prompt("Ketik pilihanmu 'Rock', 'Paper', atau 'Scissors'");
+  let result = playRound(userInput);
+
+  return result;
 }
