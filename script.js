ac4a6c2f30977fe59691afed271ba4261e095682
aaditamanugraha <adhityaaditaman@gmail.com>
2023-02-26 23:24:13 +0700

    Create playRound function

--- script.js
+++ script.js
@@ -1,7 +1,41 @@
 function getComputerChoice() {
-  const choiceOptions = ["Rock", "Paper", "Scissors"];
+  const choiceOptions = ["ROCK", "PAPER", "SCISSORS"];
   let randomNumber = Math.floor(Math.random() * 3);
   let randomChoice = choiceOptions[randomNumber];
 
-  console.log(randomChoice);
+  return randomChoice;
+}
+
+function playRound(player) {
+  let playerSelection = player.toUpperCase();
+  let computerSelection = getComputerChoice();
+
+  if (playerSelection === "ROCK" && computerSelection === "SCISSORS") {
+    console.log("You win! " + playerSelection + " beats " + computerSelection);
+  } else if (playerSelection === "ROCK" && computerSelection === "PAPER") {
+    console.log("You lose! " + computerSelection + " beats " + playerSelection);
+  } else if (playerSelection === "ROCK" && computerSelection === "ROCK") {
+    console.log(
+      "Draw! " + computerSelection + " can't beats " + playerSelection
+    );
+  } else if (playerSelection === "PAPER" && computerSelection === "ROCK") {
+    console.log("You win! " + playerSelection + " beats " + computerSelection);
+  } else if (playerSelection === "PAPER" && computerSelection === "SCISSORS") {
+    console.log("You lose! " + computerSelection + " beats " + playerSelection);
+  } else if (playerSelection === "PAPER" && computerSelection === "PAPER") {
+    console.log(
+      "Draw! " + computerSelection + " can't beats " + playerSelection
+    );
+  } else if (playerSelection === "SCISSORS" && computerSelection === "PAPER") {
+    console.log("You win! " + playerSelection + " beats " + computerSelection);
+  } else if (playerSelection === "SCISSORS" && computerSelection === "ROCK") {
+    console.log("You lose! " + computerSelection + " beats " + playerSelection);
+  } else if (
+    playerSelection === "SCISSORS" &&
+    computerSelection === "SCISSORS"
+  ) {
+    console.log(
+      "Draw! " + computerSelection + " can't beats " + playerSelection
+    );
+  }
 }
