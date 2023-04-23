ab0508f1f8d21e4776ce2302320a9967d0ae7786
aaditamanugraha <adhityaaditaman@gmail.com>
2023-04-23 18:14:55 +0700

    Catch user input

--- script.js
+++ script.js
@@ -12,6 +12,19 @@ function playerSelection() {
   let playerChoices = prompt(
     "Masukan pilihan anda! Rock, Paper, atau Scissors."
   );
+  let simplifyPlayerChoices = playerChoices.toLowerCase();
 
-  console.log(playerChoices);
+  console.log(simplifyPlayerChoices);
+
+  if (simplifyPlayerChoices === "rock") {
+    alert("Kamu memilih " + simplifyPlayerChoices);
+  } else if (simplifyPlayerChoices === "paper") {
+    alert("Kamu memilih " + simplifyPlayerChoices);
+  } else if (simplifyPlayerChoices === "scissors") {
+    alert("Kamu memilih " + simplifyPlayerChoices);
+  } else {
+    alert(
+      "Pilihan anda bukanlah sebuah pilihan! Silahkan isikan sesuai dengan instruksi."
+    );
+  }
 }
