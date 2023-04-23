5a64a59d5a4ad7431f2b2af0c81393ade89c6dd8
aaditamanugraha <adhityaaditaman@gmail.com>
2023-04-23 17:44:26 +0700

    Add random computer choices function

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
 let playerScore = 0;
 let computerScore = 0;
 
+function getComputerChoices() {
+  const choices = ["ROCK", "PAPER", "SCISSORS"];
+  const randomChoices = Math.floor(Math.random() * 3);
+
+  return choices[randomChoices];
+}
